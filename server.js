bed976bb2e8ab234d810e3cc563c59aa0a62bf02
Calteryeker <claudiomarciofilho@hotmail.com>
2022-03-08 17:21:42 -0300

    Página inicial estilizada, estrutura de pastas atualizada

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //inicia o servidor
 app.listen(3000);
 
+//permite o uso do css e javascript das páginas
+app.use(express.static(__dirname + '/frontend'));
+//Adicionar rotas == app.get('/rota', (req,res) => {});
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/backend/html/index.html');
+    res.sendFile(__dirname + '/frontend/html/index.html');
 });
