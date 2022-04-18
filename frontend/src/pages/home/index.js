16edc0abde44993558e68d190eb3e1158e733332
Cláudio Araujo <claudiomarciofilho@hotmail.com>
2022-04-18 20:23:55 -0300

    Redirecionar par Login page caso o token não seja mais válido!

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { parseCookies } from "nookies"
 import { useContext } from "react"
 import { AuthContext, AuthProvider } from "../../contexts/AuthContext"
 
@@ -5,6 +6,28 @@ export default function Index() {
   const { user } = useContext(AuthContext)
   
   return (
-        <h1>Welcome {user.nome}</h1>
-    )
+    <>
+      <Head>
+        <title>MyBookshelf | Home</title>
+      </Head>
+      <h1>Welcome {user.nome}</h1>
+    </>
+  )
+}
+
+export const getServerSideProps = async (ctx) => {
+  const { ['mybookshelf-token']: token} = parseCookies(ctx);
+   
+  if(!token){
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
   }
+
+  return {
+    props : {}
+  }
+}
