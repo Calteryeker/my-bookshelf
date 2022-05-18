aa275f5e502cb057f0ca1e2e9b115a93955971bb
Pedro Antônio de Souza Silva <pedroantoniodesouzasilva@gmail.com>
2022-05-18 13:02:07 -0300

    Styling signup page for mobile

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -44,55 +44,67 @@ export default function Signup() {
   }
 
   return (
-    <div className="pt-28 flex flex-col overflow-y-scroll items-stretch justify-between bg-signup-bg bg-no-repeat bg-cover min-h-screen ">
+    <div className="pt-28 flex flex-col overflow-y-scroll justify-between bg-signup-bg bg-no-repeat bg-cover min-h-screen ">
       <Head>
         <title>MyBookshelf | Cadastro</title>
         <link rel="icon" href="/logo.png" />
       </Head>
       <Navbar />
-      <div className="flex flex-col items-center bg-white bg-opacity-30 sm_c:mt-10 sm_c:mx-6">
+      <div className="rounded-lg pt-10 mb-10 flex flex-col items-center bg-white bg-opacity-80 sm_c:mt-10 sm_c:mx-6">
+      <img classname="sm_c:mx-auto justify-center" width={80} height={80} src="/images/logo_bg_brow.png"/>
+      <h2 className="font-luck flex items-center justify-center sm_c:mt-10 sm_c:text-2xl sm_c:text-center sm_c:mx-2 sm_c:text-brow_pod-1 hover:text-white hover:bg-orange-500 duration-500 md:py-4 md:text-lg md:px-10 ">Faça Login ou Registre-se em Segundos</h2>
+      <p className="mx-5 font-inter border-brow_pod-1 sm_c:text-center">
+        Use sua conta de email para registrar-se no mybookshelf. É gratuito!
+      </p>
         <Formik initialValues={{}} onSubmit={handleClickSignUp} validationSchema={validationSignup}>
           <Form className="signup-form">
-            <h2 className="signup-form-title">Cadastro</h2>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Nome:
-                <Field name="name" className="form-field" placeholder="Digite seu nome" />
-                <ErrorMessage component="span" name="name" className="form-error" />
+            <div className="sm_c:grid sm_c:grid-cols-1 sm_c:justify-items-center">
+            <div class="flex items-center justify-center py-8">        
+            </div>
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-0">Nome Completo:
+                <div><Field name="name" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2" placeholder="Digite seu nome" /></div>
+                <div><ErrorMessage component="span" name="name" className="form-error" /></div>
               </label>
             </div>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Username:
-                <Field name="username" className="form-field" placeholder="Digite seu username" />
-                <ErrorMessage component="span" name="username" className="form-error" />
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-0">ID Login:
+                <div><Field name="username" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2" placeholder="Digite seu username" /></div>
+                <div><ErrorMessage component="span" name="username" className="form-error" /></div>
               </label>
             </div>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Email:
-                <Field name="email" className="form-field" placeholder="Digite seu email" />
-                <ErrorMessage component="span" name="email" className="form-error" />
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-0">Email:
+                <div><Field name="email" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2" placeholder="user@email.com.br" /></div>
+                <div><ErrorMessage component="span" name="email" className="form-error" /></div>
               </label>
-            </div>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Senha:
-                <Field type="password" name="password" className="form-field" placeholder="Digite sua senha" />
-                <ErrorMessage component="span" name="password" className="form-error" />
-              </label>
-            </div>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Confirmar Senha:
-                <Field type="password" name="confirmPassword" className="form-field" placeholder="Confirme sua senha" />
-                <ErrorMessage component="span" name="confirmPassword" className="form-error" />
-              </label>
-            </div>
-            <div className="signup-form-group">
-              <label className="signup-form-field-type">Data de nascimento:
-                <DatePicker showYearDropdown dropdownMode="select" name="birthdate" selected={startDate} onChange={(date) => setStartDate(date)} inline={false} dateFormat="dd/MM/yyyy" onClickOutside={testDate} onCalendarClose={testDate} />
+              </div>
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-2">Data de Nascimento:
+                <DatePicker showYearDropdown dropdownMode="select" name="birthdate" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2" selected={startDate} onChange={(date) => setStartDate(date)} inline={false} dateFormat="dd/MM/yyyy" onClickOutside={testDate} onCalendarClose={testDate} />
                 <ErrorMessage component="span" name="birthdate" className="form-error" />
               </label>
+            
             </div>
-            <button className="button" type="submit">
-              Cadastrar
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-0">Senha:
+                <div><Field type="password" name="password" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2" placeholder="Digite sua senha" /></div>
+                <div><ErrorMessage component="span" name="password" className="form-error" /></div>
+              </label>
+            </div>
+            <div className="signup-form-group py-2">
+              <label className="sm_c:grid sm_c:grid-cols-1 sm_c:gap-0">Confirmar Senha:
+                <div><Field type="password" name="confirmPassword" className="font-inter sm_c:form-field sm_c:rounded-2xl sm_c:py-3 sm_c:px-24 sm_c:border-brow_pod-1 sm_c:border-2 sm_c:pl-2 " placeholder="Confirme sua senha" /></div>
+                <div><ErrorMessage component="span" name="confirmPassword" className="form-error" /></div>
+              </label>
+            </div>
+            <div class="flex items-center justify-center py-8">
+            <button className="button font-luck w-fit sm_c:px-16 sm_c:py-4 border-transparent sm_c:text-2xl  rounded-md text-orange-900 bg-romantic-1  hover:text-white hover:bg-orange-500 duration-500 md:py-4 md:text-lg md:px-10 " type="submit">
+              Continuar
             </button>
+            </div>
+            <p className="sm_c:mx-5 sm_c:font-inter sm_c:border-brow_pod-1 sm_c:text-left sm_c:mb-10"> Ao continuar você concorda com nossos termos de uso e políticas de privacidade.</p>
+            </div>
 
           </Form>
         </Formik>
