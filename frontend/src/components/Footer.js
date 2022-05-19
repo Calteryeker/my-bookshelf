bfef69722d553e188145f3384a5698695de9a533
Pedro Antônio de Souza Silva <pedroantoniodesouzasilva@gmail.com>
2022-05-19 18:18:18 -0300

    Finished privacy politics page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return ( 
+    return (
         <div className="flex items-center justify-around bg-brow_pod-1 h-20 ">
-            <p className="text-white text-sm text-center font-inter"><u>Termos e Privacidade</u> © Todos os direitos reservados, MyBookshelf</p>
+            <p className="text-white text-sm text-center font-inter"><a className="hover:underline" href="/privacy">Termos e Privacidade</a> © Todos os direitos reservados, MyBookshelf</p>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
