bb359f0324c13e93daa5f7511f9488ed67906e02
jhonsidney <jhon.sidney@gmail.com>
2022-05-31 20:19:21 -0300

    alterações no dashboard

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
 
-const Books = ({title, books, loading,css}) => {
+const Books = ({title, books, loading,css_componente,css_title, css_ul}) => {
   if (loading || !books) {
     return <h2>Carregando...</h2>;
   }
 
   return (
     <>
-        <div className={`sm_c:mt-14 sm_c:w-[370px] sm_c:shadow-sm sm_c:shadow-brow_pod-1 sm_c:rounded-xl sm_c:px-5 sm_c:py-7 sm_c:bg-brow_pod-1 sm_c:text-romantic-1` + css}>
-          <h2 className=" sm_c:text-2xl sm_c:font-luck sm_c:text-white md_c:text-lg ">{title}</h2>
-          <ul className="list-group t font-lekton">
+        <div className={css_componente}>
+          <h2 className={css_title}>{title}</h2>
+          <ul className={css_ul}>
           {books.map(book => (
               <li key={book._id}>
                   <Link href={`/book/${book._id}/view`} >
                       <a className='list-group-item '>
-                          {book.titulo}
+                          {book.titulo.toUpperCase()}
                       </a>
                   </Link>
               </li>
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -45,7 +45,7 @@ export default function Index() {
 
     fetchBooks(currentLocalPage)
   }, [])
-  
+
   //Controle da páginação
   const indexLastBook = currentLocalPage * booksPerPage;
   const indexFirstBook = indexLastBook - booksPerPage;
@@ -71,13 +71,13 @@ export default function Index() {
     Router.replace('/')
   }
 
-  function chargeLastBooks(lastBooks){
+  function chargeLastBooks(lastBooks) {
     return (
       lastBooks.map(book => (
         <li key={book._id}>
           <Link href={`/book/${book._id}/view`} >
             <a className='list-group-item'>
-                {book.titulo}
+              {book.titulo}
             </a>
           </Link>
         </li>
@@ -89,10 +89,10 @@ export default function Index() {
   const myBooks = () => {
     setTitle("Meus Livros:")
 
-    if (filtering){
+    if (filtering) {
       searchWithFilter(document.getElementById("textFilter").value)
     }
-    else{
+    else {
       setFilteredBooks(undefined)
       console.log("meus livros")
     }
@@ -100,39 +100,39 @@ export default function Index() {
 
   const filterReading = () => {
     setTitle("Em Leitura:")
-    if (filtering){
+    if (filtering) {
       searchWithFilter(document.getElementById("textFilter").value)
       setFilteredBooks(filteredBooks => filteredBooks.filter(book => book.estado === 1))
     }
-    else{
+    else {
       setFilteredBooks(books.filter(book => book.estado === 1))
     }
   }
 
   const filterFinished = () => {
     setTitle("Finalizados:")
-    if (filtering){
+    if (filtering) {
       searchWithFilter(document.getElementById("textFilter").value)
       setFilteredBooks(filteredBooks => filteredBooks.filter(book => book.estado === 2))
     }
-    else{
+    else {
       setFilteredBooks(books.filter(book => book.estado === 2))
-      
-    } 
+
+    }
   }
 
-  function searchWithFilter(text){
+  function searchWithFilter(text) {
     setCurrentLocalPage(1);
     const regex = new RegExp(text.toLowerCase())
-    
-    if(filter === "Gênero"){
+
+    if (filter === "Gênero") {
       setFilteredBooks(books.filter(book => book.lista_generos.filter(genero => regex.test(genero.toLowerCase())).length > 0))
     }
-    else if(filter === "Autor"){
+    else if (filter === "Autor") {
       setFilteredBooks(books.filter(book => regex.test(book.autor.toLowerCase())))
     }
-    else if(filter === "Ano"){
-      if(text == ''){
+    else if (filter === "Ano") {
+      if (text == '') {
         setFilteredBooks(books)
         return
       }
@@ -140,25 +140,25 @@ export default function Index() {
     }
   }
 
-  function callFilter(){
+  function callFilter() {
     const regex = new RegExp(title.toLowerCase())
-    if(regex.test("meus livros:")){
+    if (regex.test("meus livros:")) {
       myBooks()
     }
-    else if(regex.test("em leitura:")){
+    else if (regex.test("em leitura:")) {
       filterReading()
     }
-    else if(regex.test("finalizados:")){
+    else if (regex.test("finalizados:")) {
       filterFinished()
     }
   }
 
-  function showFilterBar(){
-    setFiltering(true); 
+  function showFilterBar() {
+    setFiltering(true);
     callFilter()
   }
 
-  function clearSearch(){
+  function clearSearch() {
     document.getElementById("textFilter").value = '';
   }
 
@@ -171,41 +171,41 @@ export default function Index() {
 
       <NavbarDash bgColor={`brow_pod-1`} tittleColor={`white`} image={"logonova2.png"} user={user} />
 
-      <div className=" sm_c:pt-0 flex sm_c:flex-col sm_c:min-h-screen sm_c:items-center sm_c:justify-around sm_c:bg-romantic-1 overflow-hidden sm_c:mt-20 md_c:items-start md_c:flex-row md_c:justify-start md_c:bg-white">
-        <div className="bg-romantic-1 md_c:flex md_c:flex-col md_c:align-top md_c:bg-white md_c:pt-20 md_c:w-56">
-          <div className="flex flex-row justify-center md_c:text-brow_pod-1 ">
-            <img src="/images/plus.png" className='sm_c:hidden md_c:block  w-7 h-7 mt-3' />
-            <Link href="book/create"><button className="sm_c:hidden md_c:block md_c:mx-2 md_c:font-luck">Novo Livro</button></Link>
+      <div className=" sm_c:pt-0 flex sm_c:flex-col sm_c:min-h-screen sm_c:items-center sm_c:justify-around sm_c:bg-romantic-1 overflow-hidden sm_c:mt-20 md_c:items-start md_c:flex-row md_c:justify-start md_c:bg-white  ">
+        <div className=" bg-romantic-1 md_c:flex md_c:flex-col md_c:align-top md_c:bg-white md_c:pt-20 md_c:w-[20%] md_c:min-h-screen ">
+          <div className="flex flex-row justify-start text-brow_pod-1 border border-orange-800 mx-9 rounded-lg py-3 ml-7 px-5 bg-romantic-1 text-[0.8em] hover:bg-green-700 hover:text-white">
+            <img src="/images/plus.png" className='sm_c:hidden md_c:block  w-7 h-7' />
+            <Link href="book/create"><button className="sm_c:hidden md_c:block md_c:mx-1 md_c:font-luck ">Novo Livro</button></Link>
           </div>
           <FilterStates status={"sm_c:hidden md_c:block md_c:font-luck"} myBooks={myBooks} filterReading={filterReading} filterFinished={filterFinished} />
-          <Books title={"Últimos Livros Cadastrados"} books={lastBooksAdded}  />
+          <Books title={"Últimos Livros Cadastrados"} books={lastBooksAdded} css_componente={"sm_c:mt-14 sm_c:w-[350px] sm_c:shadow-sm sm_c:shadow-brow_pod-1 sm_c:rounded-xl sm_c:px-5 sm_c:py-6 sm_c:bg-brow_pod-1 md_c:shadow-lg md_c:shadow-brow_pod-1 md_c:bg-white md_c:w-[90%] md_c:mx-2 "} css_title={"sm_c:mb-2 sm_c:text-2xl sm_c:font-luck sm_c:text-white md_c:text-xl md_c:text-brow_pod-1 md_c:mx-5"} css_ul={"sm_c:text-romantic-1 sm_c:font-inter md_c:text-sm md_c:font-bold md_c:text-brow_pod-1 md_c:leading-loose"} />
         </div>
-
-        <div className="w-full md_c:pt-16 flex flex-col bg-section-logo-2 bg-romantic-1">
+        
+        <div className=" md_c:shadow-local md_c:min-h-screen  md_c:w-full md_c:pt-16 flex flex-col bg-section-logo-2 bg-romantic-1">
           <p className="sm_c:hidden md_c:block flex flex-col mx-auto font-luck text-4xl text-brow_pod-1">Dashboard</p>
           <div className=" sm_c:font-luck sm_c:mx-auto sm_c:flex sm_c:flex-row sm_c:my-10  ">
-            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-cyan-300 sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-12 md_c:mx-5">
-              <h3 className="sm_c:text-lg">Cadastrados</h3>
+            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-cyan-300 sm_c:hover:bg-sky-500  sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-6 md_c:mx-2 ">
+              <h3 className="sm_c:text-lg md_c:text-3xl">Cadastrados</h3>
               <div className="sm_c:flex sm_c:flex-row ">
-                <img src="/images/bookshelf.png" width={50} height={20} className="md_c:w-16"/>
-                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 ">27</p>
+                <img src="/images/bookshelf.png" width={50} height={20} className="md_c:w-16" />
+                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 md_c:text-7xl">{books.length >= 10 ? books.length : `0` + books.length}</p>
               </div>
             </div>
-            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-white sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-12 md_c:mx-5">
-              <h3 className="sm_c:text-lg">Em leitura</h3>
+            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-white sm_c:hover:bg-green-500 sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-10 md_c:mx-2">
+              <h3 className="sm_c:text-lg md_c:text-3xl">Em leitura</h3>
               <div className="sm_c:flex sm_c:flex-row ">
-                <img src="/images/open_book.png" width={50} height={20} className="md_c:w-16"/>
-                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 ">27</p>
+                <img src="/images/open_book.png" width={50} height={20} className="md_c:w-16" />
+                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 md_c:text-7xl">{books.filter(book => book.estado === 1).length >= 10 ? books.filter(book => book.estado === 1).length : `0` + books.filter(book => book.estado === 1).length }</p>
               </div>
             </div>
-            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-yellow-200 sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-12 md_c:mx-5">
-              <h3 className="sm_c:text-lg">Finalizados</h3>
+            <div className="sm_c:shadow-lg sm_c:shadow-brow_pod-1 sm_c:mx-1 sm_c:px-1 sm_c:py-10 sm_c:bg-yellow-200 sm_c:hover:bg-yellow-400 sm_c:flex sm_c:flex-col sm_c:text-brow_pod-1 sm_c:rounded-md md_c:px-6 md_c:mx-1">
+              <h3 className="sm_c:text-lg md_c:text-3xl">Finalizados</h3>
               <div className="sm_c:flex sm_c:flex-row ">
-                <img src="/images/livros.png" width={50} height={20} className="md_c:w-16"/>
-                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 ">27</p>
+                <img src="/images/livros.png" width={50} height={20} className="md_c:w-16" />
+                <p className="sm_c:text-5xl sm_c:mt-1 sm_c:ml-2 md_c:text-7xl">{books.filter(book => book.estado === 2).length >= 10 ? books.filter(book => book.estado === 2).length : `0` + books.filter(book => book.estado === 2).length }</p>
               </div>
             </div>
-            
+
           </div>
 
           <div className="sm_c:mx-auto sm_c:w-[370px] sm_c:mb-10 sm_c:py-4 sm_c:bg-brow_pod-1 sm_c:rounded-md sm_c:px-1 md_c:py-14 md_c:flex md_c:flex-col md_c:w-[750px] md_c:ml-auto md_c:rounded-2xl">
@@ -221,22 +221,22 @@ export default function Index() {
                         <img src="/images/lupa.png" width={30} height={20} className="md_c:w-10" />
                       </div>
 
-                      <label className="sm_c:mr-2 font-luck" onClick={ setFilter(values.picked)}>
+                      <label className="sm_c:mr-2 font-luck" onClick={setFilter(values.picked)}>
                         <Field className="sm_c:mr-0 md_c:mr-2" type="radio" name="picked" value="Gênero" />
                         Gênero
                       </label>
-                      <label className="sm_c:mr-2 font-luck"  onClick={setFilter(values.picked)}>
+                      <label className="sm_c:mr-2 font-luck" onClick={setFilter(values.picked)}>
                         <Field className="sm_c:mr-0 md_c:mr-2" type="radio" name="picked" value="Autor" />
                         Autor
                       </label>
-                      <label className="sm_c:mr-2 font-luck"  onClick={setFilter(values.picked)}>
+                      <label className="sm_c:mr-2 font-luck" onClick={setFilter(values.picked)}>
                         <Field className="sm_c:mr-0 md_c:mr-2" type="radio" name="picked" value="Ano" />
                         Ano
                       </label>
                       {
                         filtering ?
                           <label className="sm_c:text-sm sm_c:ml-1 ">
-                            <button className="sm_c:py-1 sm_c:px-1 sm_c:text-white sm_c:rounded-md sm_c:border-brow_pod-1 sm_c:border-2 sm_c:bg-brow_pod-1 md_c:justify-end md_c:ml-auto" type="button" onClick={() => {values.picked = "0"; setFiltering(false); clearSearch(); callFilter()}}>Limpar Filtros</button>
+                            <button className="sm_c:py-1 sm_c:px-1 sm_c:text-white sm_c:rounded-md sm_c:border-brow_pod-1 sm_c:border-2 sm_c:bg-brow_pod-1 md_c:justify-end md_c:ml-auto" type="button" onClick={() => { values.picked = "0"; setFiltering(false); clearSearch(); callFilter() }}>Limpar Filtros</button>
                           </label> : null
                       }
                     </div>
@@ -248,7 +248,7 @@ export default function Index() {
               </Formik>
             </div>
             <FilterStates status={"md_c:hidden"} myBooks={myBooks} filterReading={filterReading} filterFinished={filterFinished} />
-            <Books title={title} books={filteredBooks ? currentFilteredBooks : currentBooks} loading={loading} />
+            <Books title={title} books={filteredBooks ? currentFilteredBooks : currentBooks} loading={loading} css_componente={"sm_c:mt-14 sm_c:w-[350px] sm_c:shadow-sm sm_c:shadow-brow_pod-1 sm_c:rounded-xl sm_c:px-5 sm_c:py-7 sm_c:bg-brow_pod-1 sm_c:text-romantic-1"} css_title={"sm_c:text-2xl sm_c:font-luck sm_c:text-white md_c:text-lg"} />
             <Pagination booksPerPage={booksPerPage} totalBooks={filteredBooks ? filteredBooks.length : books.length} paginate={paginate} />
           </div>
         </div>
